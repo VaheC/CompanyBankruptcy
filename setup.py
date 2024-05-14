c71c04fa77ce347d5242b28fcecdfa87b905318d
VaheC <vcharchian@gmail.com>
2024-05-14 07:32:45 +0400

    setup added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='company_bankruptcy',
+    version='0.0.0',
+    author='VaheCharchyan',
+    author_email='vcharchian@gmail.com',
+    packages=find_packages()
+)
