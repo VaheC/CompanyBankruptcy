eb8d37722e9c11034c18f3e501a84e7e062b177d
VaheC <vcharchian@gmail.com>
2024-05-17 22:35:40 +0400

    removed

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='company_bankruptcy',
-    version='0.0.0',
-    author='VaheCharchyan',
-    author_email='vcharchian@gmail.com',
-    packages=find_packages()
-)
